56c0dd4ac17c0c426e0279d5906c5c725c15d81b
Kyungjae-Sung <tjdrns00@gmail.com>
2026-04-15 13:36:43 +0900

    변수와 f-string 학습 및 AI 프롬프트 생성기 미션 완료

---
+++ 02.1_mission.py
@@ -0,0 +1,13 @@
+user_name = "KJ"
+task = "learning python for ai automation"
+language = "Korean"
+length = "3 lines"
+
+print(f"{user_name}를 위한 요청: {task}을 {language}로 {length}로 작성해주세요.")
+
+user_name = "KJ"
+task = "바이브코딩"
+language = "자연어"
+length = "3줄"
+
+print(f"{user_name}는 {task}을 하는데, {language}를 사용합니다. 결과는 항상 {length}이내로 받고 있습니다.")
--- 02_variables.py
+++ 02_variables.py
@@ -14,3 +14,17 @@ print(f"오늘의 목표는 [{today_task}] 이네요. 파이팅!")
 
 ## 4. f-string의 강력한 기능 (중괄호 안에서 계산도 가능)
 print(f"{name}님은 8월 이면 {age +1}이 되시네요")
+
+## 5. 변수를 만들고, f-string으로 자기소개 작성
+name = "경재"
+age = 40
+fav_food = "fries"
+
+print(f"안녕하세요. 전 {name}입니다. 나이는 {age}입니다. 가장 좋아하는 음식은 {fav_food}입니다.")
+
+## 6. 변수를 바꿔서 다른 사람 소개하기
+name = "Iron Man"
+age = 40
+fav_food = "cheese burger"
+
+print(f"안녕하세요. 전 {name}입니다. 나이는 {age}입니다. 가장 좋아하는 음식은 {fav_food}입니다.")
